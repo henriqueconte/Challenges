564b2aec41d76871ad5fb9915e020c81bec07570
henriqueconte <henriqueconte6@gmail.com>
2022-10-17 19:34:23 +0200

    Implemented problem 1207

---
+++ LeetCode/1207.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniqueOccurrences(self, arr):
+        sumDict = {}
+        occurencesDict = {}
+
+        for element in arr:
+            if str(element) in sumDict:
+                sumDict[str(element)] += 1
+            else:
+                sumDict[str(element)] = 1
+
+        occurrencesCountList = list(sumDict.values())
+
+        for element in occurrencesCountList:
+            if str(element) in occurencesDict:
+                return False
+            occurencesDict[str(element)] = 1
+
+        return True
+
+sol = Solution()
+
+print(sol.uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]))
