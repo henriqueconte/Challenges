e9487b231c3be56d2dfeb6db9650f31bbe6fd8e4
henriqueconte <henriqueconte6@gmail.com>
2023-06-04 19:02:17 -0300

    Implemented 207.py

--- LeetCode/207.py
+++ LeetCode/207.py
@@ -3,77 +3,62 @@ class Solution:
         if not prerequisites:
             return True
         
-        if ((numCourses - 1) * numCourses) / 2 > len(prerequisites):
+        maxAmountOfPrerequisites = ((numCourses - 1) * numCourses) / 2
+        if len(prerequisites) > maxAmountOfPrerequisites:
             return False
-
+        
         requisitesDict = {}
-        canFinishCourses = set()
-        visited = set()
-        foundCycle = False
 
         for element in prerequisites:
             course = str(element[0])
             requisite = str(element[1])
 
-            if course in prerequisites:
+            if course in requisitesDict:
                 requisitesDict[course].append(requisite)
             else:
                 requisitesDict[course] = [requisite]
 
-        for course in requisitesDict:
-            # if course in canFinishCourses:
-            #     continue
-            if self.validateCourse(course, visited, requisitesDict) == False:
+        approvedCourses = set()
+        analysingCourses = set()
+        def hasCycle(course):
+            if course in approvedCourses:
                 return False
+            elif course in analysingCourses:
+                return True
+            elif course not in requisitesDict:
+                approvedCourses.add(course)
+                return False
+            
+            analysingCourses.add(course)
 
+            for requisite in requisitesDict[course]:
+                if hasCycle(requisite):
+                    return True
+
+            analysingCourses.remove(course)
+            approvedCourses.add(course)
+
+            return False
+        
+        for course in requisitesDict:
+            if hasCycle(course):
+                return False
+            
         return True
 
-    def validateCourse(self, course, visited, requisitesDict):
-        if course not in requisitesDict:
-            return True
-        
-        validatedCourse = True
-        requisites = requisitesDict[course]
-        for element in requisites:
-            if element in visited:
-                return False
-            visited.add(element)
-            validatedCourse = self.validateCourse(element, visited, requisitesDict)
-        # if validatedCourse:
 
-        return validatedCourse
-        # if self.foundCycle == False:
-        #     self.canFinishCourses.add(course)
 
 solution = Solution()
-# print(solution.canFinish(2, [[1,0]]))      
-# print(solution.canFinish(2, [[1,0], [0,1]]))
-# print(solution.canFinish(4, [[1,2], [2,3], [3,4]]))            
-# print(solution.canFinish(2, [[0,1]]))  
+print(solution.canFinish(2, [[1,0]]))      
+print(solution.canFinish(2, [[1,0], [0,1]]))
+print(solution.canFinish(4, [[1,2], [2,3], [3,4]]))            
+print(solution.canFinish(2, [[0,1]]))  
 print(solution.canFinish(5, [[1,4],[2,4],[3,1],[3,2]]))  
+print(solution.canFinish(3, [[1,0],[1,2],[0,1]])) 
+print(solution.canFinish(20, [[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]] )) 
+print(solution.canFinish(8,[[1,0],[2,6],[1,7],[6,4],[7,0],[0,5]] )) 
 
-# [1, 2] -> [2, 1]
-# [2, 3] -> [3, 2]
-# [3, 1] -> [1, 3]
-    
-# 4 + 3 + 2 + 1
-# 5 + 4 + 3 + 2 + 1
-# 5 * (6) / 2 =
-
-# [0,1] [0,2], [0,3], [0,4], [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3,4]
-
-# Implementation 1:
-# Sort pre requisits list based on pair[0]
-# iterate over list, add course label to set after we finish reading pre requisits of a course
-# if we try to read a pre requisit
-
-
-# Implementation 2:
-# Build graph with rules
-# If find cycle on graph, return false
-
-
-# 0 -> 1    -
-# 1 -> 2, 3 -
-# 2 -> 3   -
-# 3 -> 4   -
+# 1 -> 4
+# 2 -> 4
+# 3 -> 1
+# 3 -> 2
