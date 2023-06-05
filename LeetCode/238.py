6986feb93d78178eeece716a2d1d7620c557c932
henriqueconte <henriqueconte6@gmail.com>
2023-06-05 15:22:24 -0300

    Implemented 238.py with O(1) space complexity

--- LeetCode/238.py
+++ LeetCode/238.py
@@ -1,23 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums):
-        result = [0 for _ in range(len(nums))]
-        leftProduct = [1 for _ in range(len(nums))]
-        rightProduct = [1 for _ in range(len(nums))]
+        result = [1 for _ in range(len(nums))]
 
         for i in range(1, len(nums)):
-            leftProduct[i] = nums[i-1] * leftProduct[i-1]
+            result[i] = result[i - 1] * nums[i - 1] 
 
-        for i in range(len(nums) -2, -1, -1):
-            rightProduct[i] = nums[i+1] * rightProduct[i+1]
-
-        for i in range(len(nums)):
-            result[i] = leftProduct[i] * rightProduct[i]
-
+        right = 1
+        for i in range(len(nums) -1, -1, -1):
+            result[i] = result[i] * right
+            right = right * nums[i]
         return result
     
 solution = Solution()
 print(solution.productExceptSelf([1,2,3,4]))
 print(solution.productExceptSelf([-1, 1, 0, -3, 3]))
 
+
+# [1,2,3,4,5,6,7,8,9]
+# [1,1,2,6,24,120,720,5040,40320,362880]
+# [0,0,0,0,0,0,0,0,362880]
 
         
