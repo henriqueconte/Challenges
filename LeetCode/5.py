f742e375306261f61278cc6472e5198a4afd6c62
henriqueconte <henriqueconte6@gmail.com>
2023-06-21 18:56:45 -0300

    Implemented 5.py

--- LeetCode/5.py
+++ LeetCode/5.py
@@ -1,26 +1,33 @@
 class Solution:
     def longestPalindrome(self, s):
-        longestPal = ""
+        longest_pal = ""
+
         for i in range(len(s)):
-            currentPal = self.palindromeFromCentre(s, i, i)
-            if len(currentPal) > len(longestPal):
-                longestPal = currentPal
+            
+            # Odd palindromes
+            current_pal = self.palindrome_from_mid(s, i, i)
+            if len(current_pal) > len(longest_pal):
+                longest_pal = current_pal
+        
+            # Even palindromes
+            current_pal = self.palindrome_from_mid(s, i, i+1)
+            if len(current_pal) > len(longest_pal):
+                longest_pal = current_pal
 
-            currentPal = self.palindromeFromCentre(s, i, i + 1)
-            if len(currentPal) > len(longestPal):
-                longestPal = currentPal
-
-        return longestPal
-
-    def palindromeFromCentre(self, s, left, right):
+        return longest_pal
+    
+    def palindrome_from_mid(self, s, left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-        return s[left + 1:right]
-        
+        return s[left+1:right]
 
-a = Solution()
-print(a.longestPalindrome("akka"))
+solution = Solution()
+print(solution.longestPalindrome("baaab"))
+print(solution.longestPalindrome("asdfbaaabdfd"))
+print(solution.longestPalindrome("cbbd"))
+print(solution.longestPalindrome("babad"))
 
-#akka
-#deified
+# Solution: 
+# Start from center and increase palindrome until it finds different characters
+# Then, divide and conquer to find the biggest palindrome on the left and on the right side
