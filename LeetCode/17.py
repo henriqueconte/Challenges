0426c5a1c78aa390cf6297e82b7238a62e9e243e
henriqueconte <henriqueconte6@gmail.com>
2023-06-26 14:36:43 -0300

    Implemented 17.py

--- LeetCode/17.py
+++ LeetCode/17.py
@@ -1,17 +1,6 @@
 class Solution:
     def letterCombinations(self, digits):
-        # Circular buffer approach
-        # offset = 0
-        # Iterate over digits:
-            # For every digit:
-                # Loop on the characters of the digit, starting from 0 + offset (ex: if offset = 1 and digit = 3, start on E.)
-                # If currentCharCount > len(charsDigit)
-                    # currenctCharCount = 0
-
-        # What if you brute force?
-
-        offset = 0
-        resultSize = 0
+        ans = []
         phoneDict = {
             "2" : ["a", "b", "c"],
             "3" : ["d", "e", "f"],
@@ -22,30 +11,21 @@ class Solution:
             "8" : ["t", "u", "v"],
             "9" : ["w", "x", "y", "z"]
         }
-        resultCount = 0
-
-        if len(digits) > 0:
-            resultSize = 1
-            for digit in digits:
-                resultSize = resultSize * (len(phoneDict[digit]))
-
-        result = ["" for x in range(resultSize)]
-
-        # ad be cf    a b c   a b c
-
+        letters = []
         for digit in digits:
-            digitLetters = phoneDict[digit]
-            for k in range(int(resultSize / len(digitLetters))):
-                for i in range(len(digitLetters)): 
-                 # Each digit is going to appear that amount of times
-                    result[resultCount] += digitLetters[(i + (k * offset)) % len(digitLetters)] # Iterate over the digits to do like "a b c a b c"
-                    resultCount += 1
-                    # print(resultCount)
-            offset += 1
-            resultCount = 0
+            letters.append(phoneDict[digit])
 
-        return result
+        self.combine_letters(letters, 0, "", ans)
+
+        return ans
+
+    def combine_letters(self, letters, index, curr_comb, ans):
+        if index < len(letters):
+            for letter in letters[index]:
+                self.combine_letters(letters, index + 1, curr_comb + letter, ans)
+                
+        if len(curr_comb) == len(letters):
+            ans.append(curr_comb)
 
 solution = Solution()
-
-print(solution.letterCombinations("27"))     
+print(solution.letterCombinations("23"))
